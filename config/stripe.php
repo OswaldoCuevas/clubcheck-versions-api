b9a1ce4b4df66ac94178617d68bf248f46908896
OswaldoCuevas <108444242+OswaldoCuevas@users.noreply.github.com>
2026-08-06 16:57:31 +0200

    planes

--- config/stripe.php
+++ config/stripe.php
@@ -35,6 +35,7 @@ return [
             ],
             'type' => 'monthly',
         ],
+        // ****************** PLANES ESSENTIALS ***************************
              'intermediate_monthly_without_whatsapp' => [
             'name' => 'Plan Essentials',
             'lookup_key' => 'intermediate_monthly_without_whatsapp',
@@ -49,98 +50,230 @@ return [
             'type' => 'monthly',
              'showBillingIds' =>['cus_UrR2Ei1DYPNutc']
         ],
-              'intermediate_monthly_without_whatsapp_2' => [
+
+              'essential_monthly_without_whatsapp' => [ // -- nuevo precio
             'name' => 'Plan Essentials',
-            'lookup_key' => 'intermediate_monthly_without_whatsapp_2',
+            'lookup_key' => 'essential_monthly_without_whatsapp',
             'rules' => [
                 'enable_fingerprint' => true,
                 'enable_qr' => true,
                 'max_messages' => 0,// 
-                'max_members_actives' => 300,
+                'max_members_actives' => 200,
                 'products_to_sale' => null,  // ilimitado
                 'max_partners' => null,       // ilimitado
             ],
             'type' => 'monthly',
         ],
-        'intermediate_monthly' => [
+
+            'essential_monthly' => [ // -- nuevo precio
+            'name' => 'Plan Essentials + WhatsApp',
+            'lookup_key' => 'essential_monthly',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 700,// 
+                'max_members_actives' => 200,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'monthly',
+        ],
+
+        'essential_yearly_without_whatsapp' => [
+            'name' => 'Plan Essentials',
+            'lookup_key' => 'essential_yearly_without_whatsapp',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 0,
+                'max_members_actives' => 200,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'yearly',
+        ],
+
+         'essential_yearly' => [
+            'name' => 'Plan Essentials + WhatsApp',
+            'lookup_key' => 'essential_yearly',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 700,
+                'max_members_actives' => 200,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'yearly',
+        ],
+
+
+        // ****************** PLANES GROWHT ***************************
+        'intermediate_monthly_without_whatsapp_2' => [
             'name' => 'Plan Growth',
+            'lookup_key' => 'intermediate_monthly_without_whatsapp_2',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 0,
+                'max_members_actives' => 400,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'monthly',
+        ],
+        
+        'intermediate_monthly' => [
+            'name' => 'Plan Growth + WhatsApp',
             'lookup_key' => 'intermediate_monthly',
             'rules' => [
                 'enable_fingerprint' => true,
                 'enable_qr' => true,
-                'max_messages' => 1200,
-                'max_members_actives' => 300,
+                'max_messages' => 1300,
+                'max_members_actives' => 400,
                 'products_to_sale' => null,  // ilimitado
                 'max_partners' => null,       // ilimitado
             ],
             'type' => 'monthly',
         ],
+
+        'intermediate_yearly_without_whatsapp' => [
+            'name' => 'Plan Growth',
+            'lookup_key' => 'intermediate_yearly_without_whatsapp',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 0,
+                'max_members_actives' => 400,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'yearly',
+        ],
    
          'intermediate_yearly' => [
-            'name' => 'Plan Growth',
+            'name' => 'Plan Growth + WhatsApp',
             'lookup_key' => 'intermediate_yearly',
             'rules' => [
                 'enable_fingerprint' => true,
                 'enable_qr' => true,
-                'max_messages' => 1200,
-                'max_members_actives' => 300,
+                'max_messages' => 1300,
+                'max_members_actives' => 400,
                 'products_to_sale' => null,  // ilimitado
                 'max_partners' => null,       // ilimitado
             ],
             'type' => 'yearly',
-        ],
-        'professional_monthly' => [
+        ]
+        ,
+         // ****************** PLANES PRO ***************************
+
+        'professional_monthly_without_whatsapp' => [
             'name' => 'Plan Pro',
-            'lookup_key' => 'professional_monthly',
+            'lookup_key' => 'professional_monthly_without_whatsapp',
             'rules' => [
                 'enable_fingerprint' => true,
                 'enable_qr' => true,
-                'max_messages' => 2000,
+                'max_messages' => 0,
                 'max_members_actives' => 600,
                 'products_to_sale' => null,  // ilimitado
                 'max_partners' => null,       // ilimitado
             ],
             'type' => 'monthly',
         ],
-        'professional_yearly' => [
-            'name' => 'Plan Pro',
-            'lookup_key' => 'professional_yearly',
+        'professional_monthly' => [
+            'name' => 'Plan Pro + WhatsApp',
+            'lookup_key' => 'professional_monthly',
             'rules' => [
                 'enable_fingerprint' => true,
                 'enable_qr' => true,
-                'max_messages' => 2000,
+                'max_messages' => 1900,
+                'max_members_actives' => 600,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'monthly',
+        ],
+        'professional_yearly_without_whatsapp' => [
+            'name' => 'Plan Pro',
+            'lookup_key' => 'professional_yearly_without_whatsapp',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 0,
                 'max_members_actives' => 600,
                 'products_to_sale' => null,  // ilimitado
                 'max_partners' => null,       // ilimitado
             ],
             'type' => 'yearly',
         ],
-        // 'business_monthly' => [
-        //     'name' => 'Plan Business',
-        //     'lookup_key' => 'business_monthly',
-        //     'rules' => [
-        //         'enable_fingerprint' => true,
-        //         'enable_qr' => true,
-        //         'max_messages' => 1600,
-        //         'max_members_actives' => 500,
-        //         'products_to_sale' => null,  // ilimitado
-        //         'max_partners' => null,       // ilimitado
-        //     ],
-        //     'type' => 'monthly',
-        // ],
-        // 'business_yearly' => [
-        //         'name' => 'Plan Business',
-        //         'lookup_key' => 'business_yearly',
-        //         'rules' => [
-        //             'enable_fingerprint' => true,
-        //             'enable_qr' => true,
-        //             'max_messages' => 1600,
-        //             'max_members_actives' => 500,
-        //             'products_to_sale' => null,  // ilimitado
-        //             'max_partners' => null,       // ilimitado
-        //         ],
-        //         'type' => 'yearly',
-        // ],
+        'professional_yearly' => [
+            'name' => 'Plan Pro + WhatsApp',
+            'lookup_key' => 'professional_yearly',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 1900,
+                'max_members_actives' => 600,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'yearly',
+        ],
+
+        // ****************** PLANES BUSSINESS ***************************
+
+        'business_monthly_without_whatsapp' => [
+            'name' => 'Plan Business',
+            'lookup_key' => 'business_monthly_without_whatsapp',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 0,
+                'max_members_actives' => 1000,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'monthly',
+        ],
+        'business_monthly' => [
+            'name' => 'Plan Business + WhatsApp',
+            'lookup_key' => 'business_monthly',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 3100,
+                'max_members_actives' => 1000,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'monthly',
+        ],
+        'business_yearly_without_whatsapp' => [
+                'name' => 'Plan Business',
+                'lookup_key' => 'business_yearly_without_whatsapp',
+                'rules' => [
+                    'enable_fingerprint' => true,
+                    'enable_qr' => true,
+                    'max_messages' => 0,
+                    'max_members_actives' => 1000,
+                    'products_to_sale' => null,  // ilimitado
+                    'max_partners' => null,       // ilimitado
+                ],
+                'type' => 'yearly',
+        ],
+         'business_yearly' => [
+                'name' => 'Plan Business + WhatsApp',
+                'lookup_key' => 'business_yearly',
+                'rules' => [
+                    'enable_fingerprint' => true,
+                    'enable_qr' => true,
+                    'max_messages' => 3100,
+                    'max_members_actives' => 1000,
+                    'products_to_sale' => null,  // ilimitado
+                    'max_partners' => null,       // ilimitado
+                ],
+                'type' => 'yearly',
+        ],
         // 'enterprise_monthly' => [
         //     'name' => 'Plan Enterprise',
         //     'lookup_key' => 'enterprise_monthly',
