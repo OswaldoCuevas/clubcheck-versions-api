e67ae3858420209ec18549a305392219f1a5278e
OswaldoCuevas <108444242+OswaldoCuevas@users.noreply.github.com>
2026-08-17 14:19:57 -0700

    precio habilitado para cliente

--- config/stripe.php
+++ config/stripe.php
@@ -65,19 +65,20 @@ return [
             'type' => 'monthly',
         ],
 
-        //     'essential_monthly_2' => [ // -- nuevo precio
-        //     'name' => 'Plan Essentials + WhatsApp',
-        //     'lookup_key' => 'essential_monthly_2',
-        //     'rules' => [
-        //         'enable_fingerprint' => true,
-        //         'enable_qr' => true,
-        //         'max_messages' => 700,// 
-        //         'max_members_actives' => 200,
-        //         'products_to_sale' => null,  // ilimitado
-        //         'max_partners' => null,       // ilimitado
-        //     ],
-        //     'type' => 'monthly',
-        // ],
+            'essential_monthly_2' => [ // -- nuevo precio
+            'name' => 'Plan Essentials + WhatsApp',
+            'lookup_key' => 'essential_monthly_2',
+            'rules' => [
+                'enable_fingerprint' => true,
+                'enable_qr' => true,
+                'max_messages' => 700,// 
+                'max_members_actives' => 200,
+                'products_to_sale' => null,  // ilimitado
+                'max_partners' => null,       // ilimitado
+            ],
+            'type' => 'monthly',
+             'showBillingIds' =>['cus_V1DrclGADRGXzT']
+        ],
 
         'essential_yearly_without_whatsapp' => [
             'name' => 'Plan Essentials',
